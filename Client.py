fd714eebe8b881baada8357826e22390028170e0
Omar Abdulbagi <ramoboy56@gmail.com>
2023-02-07 19:58:32 +0300

    added comments

--- Client.py
+++ Client.py
@@ -1,6 +1,6 @@
 import socket
 
-def readChar():
+def readChar(): # this function reads charecters and checks if input is correct or not
     while(True):
         char = input("Enter a Character to be searched: ")
         if ((len(char) > 1) or (len(char)==0)):
@@ -8,7 +8,7 @@ def readChar():
             continue
         else: return char
 
-def readString():
+def readString(): # this funciton reads the String and checks if any empty input is entered
     while(True):
         string = input("Enter a String: ")
         if(len(string)== 0):
@@ -16,7 +16,7 @@ def readString():
             continue
         else: return string
 
-def terminate():
+def terminate(): #this checks if the program needs to be terminated by only taking "Y" and "N"
     while(True):
         yesOrNo = input("Want to repeat (Y/N): ") 
         if(yesOrNo == "N" or yesOrNo == "Y" ):
@@ -28,19 +28,21 @@ def terminate():
 HOST = socket.gethostname()  # The server's hostname or IP address
 PORT = 5000  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:   #here we are initializing the socket using internet adress family IPv4
+    s.connect((HOST, PORT)) #connects to server using both host and port
     
     while True:
+        # read the inputs 
         char = readChar()
         string = readString()
 
+        #send the inputs to the server
         s.send(char.encode())
         s.send(string.encode())
-
-        recieve = s.recv(1024).decode()
-        print(f"The number of Occurrences are: {recieve}")
-        if terminate() == "N":
+       
+        recieve = s.recv(1024).decode() #recieve the responce (number of occurences) from the server
+        print(f"The number of Occurrences are: {recieve}") #print result
+        if terminate() == "N": #if user doesnt want to continue terminate program
             break
 print("Thank you!")
 
--- Server.py
+++ Server.py
@@ -4,10 +4,11 @@ import socket
 import selectors
 import types
     
-HOST = socket.gethostname()
-PORT = 5000
+HOST = socket.gethostname() # The server's hostname or IP address
+PORT = 5000 # The port used by the server
 
-def numOfOccurance(key, String):
+
+def numOfOccurance(key, String): #function to find number of occurances in a string
     num=0
     for char in String:
         if(char == key):
@@ -16,15 +17,17 @@ def numOfOccurance(key, String):
 
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen(1)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: #here we are initializing the socket using internet adress family IPv4
+    s.bind((HOST, PORT)) #creates a socket server with the given host and port  
+    s.listen(1) #listenes to clients for connection
 
-    conn, adr = s.accept()
+    conn, adr = s.accept() # blocks execution and waits for an incoming connection.
     with conn:
         print(f"connected by {adr}")
         while True:
-            Char = conn.recv(1024).decode()
+            #get inputs from client
+            Char = conn.recv(1024).decode() 
             String = conn.recv(1024).decode()
-            numOfOccurences= numOfOccurance(Char,String)
-            conn.send(str.encode(str(numOfOccurences)))
+            
+            numOfOccurences= numOfOccurance(Char,String) #get num of occurences
+            conn.send(str.encode(str(numOfOccurences))) #send result to client to be printed
